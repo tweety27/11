fc51c7b4b11ba1f1813f365150c8e4db5dfbf105
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-11-16 15:59:05 +0900

    Update main.c 05

--- main.c
+++ main.c
@@ -3,22 +3,20 @@
 
 int main(void)
 {
-    int i;
-    int grade[5];
-    int average = 0;
-    for(i=0; i<5; i++)
-    {
-        printf("input value (%i) = ", i);
-        scanf("%d", &grade[i]);
-    }
+    char *pc;
+    int *pi;
+    double *pd;
 
-    for(i=0; i<5; i++)
-    {
-        printf("grade[%i] = %i\n", i, *(grade+i));
-        average = average + *(grade+i);
-    }
+    pc = (char *)10000;
+    pi = (int*)10000;
+    pd = (double *)10000;
 
-    printf("average : %i\n", average / 5);
+    printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+
+    pc++;
+    pi++;
+    pd++;
+    printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 
     return 0;
 }
