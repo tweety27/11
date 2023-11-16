781aea4be3c266c8d0ddceee97ab2889f80bc31a
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-11-16 15:29:21 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 10;
+    char c = 'a';
+
+    int* iptr = &i;
+    char* cptr = &c;
+    int* iptr2 = iptr;
+
+    printf("i: %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
+    printf("c: %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
+    printf("iptr2: %p %i \n", iptr2, *iptr2);
+
+    return 0;
+}
